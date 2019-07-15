290dd3dc8025ce0dae4e3b45e604605cb6f62d0f
Peter <peteryuanpan@gmail.com>
2019-07-15 12:17:57 +0800

    Update VideoReview.java

--- Atlab/VideoReview/VideoReview.java
+++ Atlab/VideoReview/VideoReview.java
@@ -1,98 +1,38 @@
-import com.google.gson.Gson;
+package ai;
+
 import com.qiniu.http.Client;
+import com.qiniu.http.Response;
 import com.qiniu.util.Auth;
 import com.qiniu.util.StringMap;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class ResourcesCensor {
-    //设置好账号的ACCESS_KEY和SECRET_KEY
-    private static final String ACCESS_KEY = "填写你们自己的ak";
-    private static final String SECRET_KEY = "填写你们自己的sk";
-    private static final Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
-
-    public static void main(String args[]) {
-        String result;
-        //result = ImageCensor();
-        result = VideoCensor();
-        System.out.printf(result);
-    }
-
-    //参考api文档 https://developer.qiniu.com/dora/manual/4252/image-review
-    //图片审核
-    public static String ImageCensor() {
-        // 构造post请求body
-        Gson gson = new Gson();
-
-        Map<String, Object> uri = new HashMap<>();
-        uri.put("uri", "http://oayjpradp.bkt.clouddn.com/Audrey_Hepburn.jpg");
-
-        Map<String, Object> scenes = new HashMap<>();
-        //pulp 黄  terror 恐  politician 敏感人物
-        String[] types = {"pulp", "terror", "politician", "ads"};
-        scenes.put("scenes", types);
-
-        Map params = new HashMap();
-        params.put("data", uri);
-        params.put("params", scenes);
-
-        String paraR = gson.toJson(params);
-        byte[] bodyByte = paraR.getBytes();
-
-        // 接口请求地址
-        String url = "http://ai.qiniuapi.com/v3/image/censor";
-
-        return post(url, bodyByte);
-    }
-
-    //参考api文档 https://developer.qiniu.com/dora/manual/4258/video-pulp
-    //视频审核
-    public static String VideoCensor() {
-        // 构造post请求body
-        Gson gson = new Gson();
-
-        Map bodyData = new HashMap();
-
-        Map<String, Object> uri = new HashMap<>();
-        uri.put("uri", "https://mars-assets.qnssl.com/scene.mp4");
-
-        Map<String, Object> params = new HashMap<>();
-
-        Map<String, Object> scenes = new HashMap<>();
-        //pulp 黄  terror 恐  politician 敏感人物
-        String[] types = {"pulp", "terror", "politician"};
-
-        Map<String, Object> cut_param = new HashMap<>();
-        cut_param.put("interval_msecs", 500);
-
-        params.put("scenes", types);
-        params.put("cut_param", cut_param);
-
-        bodyData.put("data", uri);
-        bodyData.put("params", params);
-        String paraR = gson.toJson(params);
-        byte[] bodyByte = paraR.getBytes();
-
-        // 接口请求地址
-        String url = "http://ai.qiniuapi.com/v3/video/censor";
-        return post(url, bodyByte);
-    }
-
-    private static String post(String url, byte[] body) {
-        String accessToken = (String) auth.authorizationV2(url, "POST", body, "application/json")
-                .get("Authorization");
-
-        Client client = new Client();
+ 
+public class VideoReview {
+     
+    static final String accessKey = "<ak>";
+    static final String secretKey = "<sk>";
+    static final String url = "http://ai.qiniuapi.com/v3/video/censor";
+    static final String video_url = "https://mars-assets.qnssl.com/scene.mp4";
+    
+    public static void main(String[] args) {
         StringMap headers = new StringMap();
-        headers.put("Authorization", accessToken);
+        headers.put("Authorization", qiniuToken());
         try {
-            com.qiniu.http.Response resp = client.post(url, body, headers, Client.JsonMime);
-            return resp.bodyString();
+            Client client = new Client();
+            Response resp = client.post(url, json().getBytes(), headers, Client.JsonMime);
+            System.out.println(resp.toString());
+            System.out.println(resp.bodyString());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
     }
-
+    
+    static String qiniuToken() {
+        Auth auth = Auth.create(accessKey, secretKey);
+        String authorization = (String) auth.authorizationV2(url, "POST", json().getBytes(), Client.JsonMime).get("Authorization");
+        System.out.println(authorization);
+        return authorization;
+    }
+     
+    static String json() {
+        return "{\"data\": {\"uri\": \"" + video_url  + "\"},\"params\": {\"scenes\": [\"pulp\"]}}";
+    }
 }
